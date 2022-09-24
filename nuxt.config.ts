6216efc688bce2807b3e83f8f5e8d2689a31fd75
DidaS <didasoficial@gmail.com>
2022-09-24 22:35:59 +0100

    Added nuxt3

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+import { defineNuxtConfig } from "nuxt/config";
+
+export default defineNuxtConfig({
+    //! Comment if "Take Over Mode" is disabled or you dont have the Volar plugin extension
+    typescript: {
+        shim: false
+    }
+})
