18685e39beb1f2607863d190bb37939bc5d93e41
DidaS <didasoficial@gmail.com>
2022-09-25 00:19:52 +0100

    Properly set up nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
-import { defineNuxtConfig } from "nuxt/config";
-
 export default defineNuxtConfig({
     //! Comment if "Take Over Mode" is disabled or you dont have the Volar plugin extension
     typescript: {
+        strict: true,
+        typeCheck: true,
+
         shim: false
-    }
+    },
+    css: ["~/styles.css"],
+    ignorePrefix: "src/"
 })
